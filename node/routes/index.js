6e5b03b673098715566d6ec2289968e5527de1a6
Thomas <thomroch69@gmail.com>
2020-08-16 23:49:14 +0200

    Revert "Fix updated response object"
    This reverts commit 19b9dc5e2d09f3875a77b6d03ac365989cf6a72c.

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,10 +1,9 @@
-Copier
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Hello Git!' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-module.exports = router; 
+module.exports = router;
